5df50bbd939442f3fe1ae733f6c633cb41ca1e7d
pwirth <wirth@hallowelt.biz>
2019-10-16 14:55:17 +0200

    Remove author email address
    [3.2]
    
    Change-Id: I6af15cf7a9cd4607e40129f244b21fbfdbe7b4a6

--- src/Extension.php
+++ src/Extension.php
@@ -19,7 +19,7 @@
  * This file is part of BlueSpice MediaWiki
  * For further information visit http://www.bluespice.com
  *
- * @author     Patric Wirth <wirth@hallowelt.com>
+ * @author     Patric Wirth
  * @package    BlueSpice Free
  * @subpackage BlueSpiceConfigManager
  * @copyright  Copyright (C) 2016 Hallo Welt! GmbH, All rights reserved.
