f6970f7b44ebb52be411b6294ace9ffafce2b324
miriam schlindwein <schlindwein@hallowelt.com>
2021-08-04 08:37:30 +0200

    Add extension to SkinSlot GlobalActionsManager
    [4.x]
    
    Change-Id: If27c6bcab4234b9a7fce3c632741df297d3e5b9f

---
+++ src/GlobalActionsManager.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace BlueSpice\ConfigManager;
+
+use Message;
+use MWStake\MediaWiki\Component\CommonUserInterface\Component\RestrictedTextLink;
+use SpecialPage;
+
+class GlobalActionsManager extends RestrictedTextLink {
+
+	/**
+	 *
+	 */
+	public function __construct() {
+		parent::__construct( [] );
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getId(): string {
+		return 'ga-bs-configmanager';
+	}
+
+	/**
+	 *
+	 * @return string[]
+	 */
+	public function getPermissions(): array {
+		$permissions = [
+			'bluespiceconfigmanager-viewspecialpage'
+		];
+		return $permissions;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getHref(): string {
+		$tool = SpecialPage::getTitleFor( 'BlueSpiceConfigManager' );
+		return $tool->getLocalURL();
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getText(): Message {
+		return Message::newFromKey( 'bs-bluespiceconfigmanager-admintool-label' );
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getTitle(): Message {
+		return Message::newFromKey( 'bs-bluespiceconfigmanager-desc' );
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getAriaLabel(): Message {
+		return Message::newFromKey( 'bs-bluespiceconfigmanager-admintool-label' );
+	}
+}
---
+++ src/HookHandler/Main.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BlueSpice\ConfigManager\HookHandler;
+
+use BlueSpice\ConfigManager\GlobalActionsManager;
+use MWStake\MediaWiki\Component\CommonUserInterface\Hook\MWStakeCommonUIRegisterSkinSlotComponents;
+
+class Main implements MWStakeCommonUIRegisterSkinSlotComponents {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onMWStakeCommonUIRegisterSkinSlotComponents( $registry ): void {
+		$registry->register(
+			'GlobalActionsManager',
+			[
+				'special-bluespice-configmanager' => [
+					'factory' => static function () {
+						return new GlobalActionsManager();
+					}
+				]
+			]
+		);
+	}
+}
