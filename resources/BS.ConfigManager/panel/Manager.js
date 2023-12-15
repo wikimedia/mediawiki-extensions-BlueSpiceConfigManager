f3d0b8a8f01575794b71071c718c9cd187d7ef4e
Daniel Riedmüller <daniel.riedmueller@hallowelt.com>
2023-12-15 11:11:39 +0100

    Label for input fields in config manager
    ERM31396
    
    Change-Id: I71594a73c9ca0638a83af5a3cf106cb9930358ad

--- resources/BS.ConfigManager/panel/Manager.js
+++ resources/BS.ConfigManager/panel/Manager.js
@@ -43,6 +43,9 @@ Ext.define( 'BS.ConfigManager.panel.Manager', {
 
 		this.cbMainPath = Ext.create( 'Ext.form.field.ComboBox', {
 			store: this.storeMainPath,
+			fieldLabel: mw.message( 'bs-configmanager-mainpath' ).plain(),
+			labelAlign: 'top',
+			labelCls: 'bs-configmanager-extjs-label',
 			valueField: 'mainpath',
 			displayField: 'mainpathdisplay',
 			queryMode: 'local',
@@ -65,7 +68,10 @@ Ext.define( 'BS.ConfigManager.panel.Manager', {
 		});
 
 		this.tfSearch = Ext.create( 'Ext.form.field.Text', {
-			flex: 4
+			flex: 4,
+			fieldLabel: mw.message( 'bs-configmanager-search' ).plain(),
+			labelCls: 'bs-configmanager-extjs-label',
+			labelAlign: 'top',
 		});
 		this.tfSearch.on( 'change', me.onSearchFieldChanged, this );
 
@@ -175,6 +181,10 @@ Ext.define( 'BS.ConfigManager.panel.Manager', {
 				backgroundColor: '#FFFFFF',
 				backgroundImage: 'none'
 			},
+			layout: {
+				type: 'hbox',
+				align: 'bottom',
+			},
 			items: this.makeTbarItems()
 		});
 	},
@@ -182,9 +192,7 @@ Ext.define( 'BS.ConfigManager.panel.Manager', {
 	makeTbarItems: function() {
 		var arrItems = [];
 		arrItems.push( this.cbMainPath );
-		arrItems.push( '|' );
 		arrItems.push( this.tfSearch );
-		arrItems.push( '|' );
 		arrItems.push( this.btnReset );
 		arrItems.push( this.btnOK );
 		return arrItems;
