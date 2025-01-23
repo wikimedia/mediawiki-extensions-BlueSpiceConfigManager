31551854908bc218c37c03b82713f3173f96850d
hslater <hamish.slater@hallowelt.com>
2025-01-23 21:34:03 +0100

    Update IContextSource namespace
    ERM38444
    
    [REL1_43, master]

--- src/Data/ConfigManager/Reader.php
+++ src/Data/ConfigManager/Reader.php
@@ -4,6 +4,7 @@ namespace BlueSpice\ConfigManager\Data\ConfigManager;
 
 use BlueSpice\ConfigDefinitionFactory;
 use MediaWiki\Context\IContextSource;
+use MediaWiki\ContextIContextSource;
 use MWStake\MediaWiki\Component\DataStore\ReaderParams;
 use Wikimedia\Rdbms\LoadBalancer;
 
@@ -19,7 +20,7 @@ class Reader extends \BlueSpice\Data\Settings\Reader {
 	 *
 	 * @param ConfigDefinitionFactory $factory
 	 * @param LoadBalancer $loadBalancer
-	 * @param \IContextSource|null $context
+	 * @param IContextSource|null $context
 	 */
 	public function __construct( ConfigDefinitionFactory $factory, $loadBalancer,
 		IContextSource $context = null ) {
