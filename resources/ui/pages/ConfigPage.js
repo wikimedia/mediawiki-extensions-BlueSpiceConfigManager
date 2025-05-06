991373c95e2ea47084ec47dea44456cf6f0d6b3b
hslater <hamish.slater@hallowelt.com>
2025-05-06 15:09:50 +1200

    eslint
    ERM20742
    
    [REL1_43, REL1_43-5.0.x, master]
    
    Change-Id: I8ab23f7f7f292a411d6529eb1519683b9db43e43
    (cherry picked from commit 1eefdd0090b121d7788f9fa9a4d900a65cd86ce8)

--- resources/ui/pages/ConfigPage.js
+++ resources/ui/pages/ConfigPage.js
@@ -21,58 +21,58 @@ bs.configmanager.ui.pages.ConfigPage.prototype.setupOutlineItem = function () {
 bs.configmanager.ui.pages.ConfigPage.prototype.setupWidget = function () {
 	this.oouiWidgets = {};
 
-	var contentClass = this.htmlClassPrefix + "content";
-	if( this.configs.length < 1 ) {
+	let contentClass = this.htmlClassPrefix + 'content';
+	if ( this.configs.length < 1 ) {
 		contentClass += ' ' + this.htmlClassPrefix + 'noentry';
 		this.$element.append(
-			'<div class="' + contentClass + '">'
-			+ '<h4>'
-			+ mw.message( 'bs-configmanager-noentry' ).plain()
-			+ '</h4>'
-			+ '</div>'
+			'<div class="' + contentClass + '">' +
+			'<h4>' +
+			mw.message( 'bs-configmanager-noentry' ).plain() +
+			'</h4>' +
+			'</div>'
 		);
 		return;
 	}
 
-	var content = '<form id="bs-configmanager-form" class="' + contentClass + '">';
-	var currentPath = '', sectionClass = '';
-	for( var i = 0; i < this.configs.length; i++ ) {
-		this.configs[i].paths.forEach( function( path ) {
-			var sections = path.split( '/' );
+	let content = '<form id="bs-configmanager-form" class="' + contentClass + '">';
+	let currentPath = '', sectionClass = '';
+	for ( let i = 0; i < this.configs.length; i++ ) {
+		this.configs[ i ].paths.forEach( ( path ) => { // eslint-disable-line no-loop-func
+			const sections = path.split( '/' );
 			if ( this.key !== sections[ 1 ] ) {
 				return;
 			}
-			if( currentPath !== sections[2] ) {
-				if( i > 0 ) {
+			if ( currentPath !== sections[ 2 ] ) {
+				if ( i > 0 ) {
 					content += '</fieldset>';
 				}
-				sectionClass = this.htmlClassPrefix
-					+ 'section '
-					+ this.htmlClassPrefix
-					+ 'section-'
-					+ ( sections[2] || 'unknown' );
+				sectionClass = this.htmlClassPrefix +
+					'section ' +
+					this.htmlClassPrefix +
+					'section-' +
+					( sections[ 2 ] || 'unknown' );
 				content += '<fieldset class="' + sectionClass + '" >';
-				if( sections[2] ) {
-					currentPath = sections[2];
+				if ( sections[ 2 ] ) {
+					currentPath = sections[ 2 ];
 					if ( this.pathNames[ currentPath ] ) {
-						var label = mw.message( this.pathNames[ currentPath ] ).text();
+						const label = mw.message( this.pathNames[ currentPath ] ).text(); // eslint-disable-line mediawiki/msg-doc
 						content += '<legend>' + label + '</legend>';
 					} else {
 						content += '<legend>' + currentPath + '</legend>';
 					}
 				}
 			}
-		}.bind( this ) );
-		content += '<div>' + this.configs[i].form + '</div>';
+		} );
+		content += '<div>' + this.configs[ i ].form + '</div>';
 
 		// Check if html contain infusable OOUI element
-		var widgetId = this.getOOUIWidgetElementId(
-			this.configs[i].var_name,
-			this.configs[i].form
+		const widgetId = this.getOOUIWidgetElementId(
+			this.configs[ i ].var_name,
+			this.configs[ i ].form
 		);
-		if( widgetId ) {
+		if ( widgetId ) {
 			// At this point, we can only get widget ID, not the instance
-			this.oouiWidgets[ this.configs[i].s_name ] = widgetId;
+			this.oouiWidgets[ this.configs[ i ].s_name ] = widgetId;
 		}
 	}
 	content += '</fieldset>';
@@ -80,47 +80,43 @@ bs.configmanager.ui.pages.ConfigPage.prototype.setupWidget = function () {
 	this.$element.append( content );
 };
 
-bs.configmanager.ui.pages.ConfigPage.prototype.infuseWidgets = function( content ) {
-	for ( let field in this.oouiWidgets ) {
+bs.configmanager.ui.pages.ConfigPage.prototype.infuseWidgets = function ( content ) {
+	for ( const field in this.oouiWidgets ) {
 		let widgetId = 'unknown';
 		try {
-			widgetId = this.oouiWidgets[field];
-			let widget = OO.ui.infuse( $( content ).find( '#' + widgetId ) );
-
-			const parent = document.querySelectorAll(
-				".bs-configmanager-configpanel-section"
-			);
+			widgetId = this.oouiWidgets[ field ];
+			const widget = OO.ui.infuse( $( content ).find( '#' + widgetId ) );
 
 			// Now we are able to replace widget id with an instance
 			this.oouiWidgets[ field ] = widget;
-			this.oouiWidgets[ field ].on( 'change', function() {
+			this.oouiWidgets[ field ].on( 'change', () => {
 				this.emit( 'change' );
-			}.bind( this ) );
-		 } catch( e ) {
-			console.error( 'Widget with id "' + widgetId + '" could not be infused', e );
+			} );
+		} catch ( e ) {
+			console.error( 'Widget with id "' + widgetId + '" could not be infused', e ); // eslint-disable-line no-console
 			continue;
 		}
 	}
 };
 
-bs.configmanager.ui.pages.ConfigPage.prototype.getOOUIWidgetElementId = function( configVar, html ) {
-	var $widget = $( html ).find( '#' + configVar + '.oo-ui-widget' );
-	if( $widget.length === 0 ) {
+bs.configmanager.ui.pages.ConfigPage.prototype.getOOUIWidgetElementId = function ( configVar, html ) {
+	const $widget = $( html ).find( '#' + configVar + '.oo-ui-widget' );
+	if ( $widget.length === 0 ) {
 		return false;
 	}
 
-	if( ! $widget.data( 'ooui' ) ) {
+	if ( !$widget.data( 'ooui' ) ) {
 		// No infusion data
 		return false;
 	}
 	return $widget.attr( 'id' );
 };
 
-bs.configmanager.ui.pages.ConfigPage.prototype.getData = function() {
-	var data = {};
-	for ( let field in this.oouiWidgets ) {
-		var value = this.oouiWidgets[ field ].getValue();
-		if( this.oouiWidgets[ field ] instanceof OO.ui.CheckboxInputWidget ) {
+bs.configmanager.ui.pages.ConfigPage.prototype.getData = function () {
+	const data = {};
+	for ( const field in this.oouiWidgets ) {
+		let value = this.oouiWidgets[ field ].getValue();
+		if ( this.oouiWidgets[ field ] instanceof OO.ui.CheckboxInputWidget ) {
 			value = this.oouiWidgets[ field ].isSelected();
 		}
 		data[ field ] = value;
